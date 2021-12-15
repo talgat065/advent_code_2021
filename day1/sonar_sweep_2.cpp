b65cb66b223493dc0be71d767515d14f13615ef3
talgat065 <talgat06585@gmail.com>
2021-12-16 01:22:59 +0600

    day 1: part 2

---
+++ day1/sonar_sweep_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+int find_increases() {
+    std::ifstream input("input.txt");
+
+    std::vector<int> lines;
+
+    int line;
+    while (input >> line) {
+        lines.push_back(line);
+    }
+
+    int result = 0;
+    int prev_three_measurement_sum = 0;
+
+    for (std::size_t i = 0; i != lines.size(); ++i) {
+        int three_measurement_sum = 0;
+
+        for (int j = i; j < i + 2; ++j) {
+            three_measurement_sum += lines[j];
+        }
+
+        if (three_measurement_sum > prev_three_measurement_sum) {
+            result++;
+        }
+
+        prev_three_measurement_sum = three_measurement_sum;
+    }
+
+    return result - 1;
+}
+
+int main () {
+    std::cout << find_increases() << std::endl;
+}
+
