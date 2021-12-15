fbb9be515bc258c57798a25999ce6cf584f21461
talgat065 <talgat06585@gmail.com>
2021-12-15 16:53:15 +0600

    init & day1

---
+++ day1/sonar_sweep.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "fstream"
+
+int find_increases() {
+    std::ifstream input("input.txt");
+
+    int line;
+    int result = 0;
+    int previous_line = 0;
+
+    while (input >> line) {
+        if (line > previous_line) {
+            result++;
+        }
+
+        previous_line = line;
+    }
+
+    return result - 1;
+}
+
+int main () {
+    std::cout << part1() << std::endl;
+}
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My C++ solutions for Advent of Code 2021 puzzles" << std::endl;
+    return 0;
+}
